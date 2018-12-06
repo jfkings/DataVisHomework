46abb4d75d0627d322eecfc9f8ca4028d13cd4ef
max power <42195495+jfkings@users.noreply.github.com>
2018-12-06 17:58:00 -0600

    Adding Python Homework

---
+++ PythonHomework/PyBank2.py
@@ -0,0 +1,128 @@
+# Dependencies
+import os
+import csv
+
+# Specify file path
+budget_data_csv = os.path.join("..", "PythonHomework", "budget_data.csv")
+
+# Open the file. Specify Varialbe to hold the contents as csvfile
+with open(budget_data_csv, 'r', newline='') as csvfile:
+    
+    #parse csv file into dict
+    csvreader = csv.DictReader(csvfile) 
+    
+    #####for row in csvreader:
+        #####print(row['Date'], row['Profit/Losses']) - Test output
+    
+    # count months by row
+    row_count = sum(1 for row in csvreader) 
+    #####print(row_count) 
+    
+with open(budget_data_csv, 'r', newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile)
+    
+    #parse 'Profit/Losses' from dict as int in list
+    pnl = [int(row['Profit/Losses']) for row in csvreader]
+    
+    #sum pnl 
+    total_pnl = sum(pnl) 
+    #####print(total_pnl)
+
+with open(budget_data_csv, 'r', newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile)
+    
+    # parse 'Profit/Losses' from dict as int in list
+    data = [int(row['Profit/Losses']) for row in csvreader]
+    #####print(data) - Test output
+
+    #get all monthly changes from list
+    monthly_change = [data[i] - data[i - 1] for i in range(1, len(data))]
+    
+    #####print(monthly_change) -Test output
+    
+    #average monthly change over all months
+    average_monthly_change = round(sum(monthly_change)/(row_count - 1), 2) 
+    #####print(average_monthly_change) 
+
+    #Get max change in monthly change list
+    max_change = max(monthly_change)
+    
+    #Locate index value of max change
+    max_change_location = monthly_change.index(max_change)
+    #####print(max_change_location, max_change)
+
+    #Get min change in monthly change list
+    min_change = min(monthly_change)
+    
+    #Locate index value of min change
+    min_change_location = monthly_change.index(min_change)
+    #####print(min_change_location, min_change)
+
+    
+with open(budget_data_csv, 'r', newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile)
+    
+    #parse 'Date' from dict as string in list
+    data_date =  [str(row['Date']) for row in csvreader]
+    ##### print(data_date) - Test output
+    
+    #find max change date using index location of max_change_location
+    max_change_date = data_date[max_change_location + 1]
+    #####print(max_change_date)
+
+    #find min change date using index location of min_change_location
+    min_change_date = data_date[min_change_location + 1]
+    #####print(min_change_date)
+
+#---------------------------------------------------------------------------
+
+#Output
+
+#print to Terminal
+print('')
+print('Financial Analysis')
+print('---------------------')
+print('Total Months: ' + str(row_count))
+print('Total: ' + '$ ' + str(total_pnl))
+print('Average Change: ' + '$ ' + str(average_monthly_change))
+print('Greatest Increase in Profits: ' + str(max_change_date) + ' $ ' + str(max_change))
+print('Greatest Decrease in Profits: ' + str(min_change_date) + ' $ ' + str(min_change))
+print('')
+
+
+#print to TextFile
+import sys
+sys.stdout=open("output.txt","w")
+print('')
+print('Financial Analysis')
+print('---------------------')
+print('Total Months: ' + str(row_count))
+print('Total: ' + '$ ' + str(total_pnl))
+print('Average Change: ' + '$ ' + str(average_monthly_change))
+print('Greatest Increase in Profits: ' + str(max_change_date) + ' $ ' + str(max_change))
+print('Greatest Decrease in Profits: ' + str(min_change_date) + ' $ ' + str(min_change))
+print('')
+sys.stdout.close()
+
+
+    
+
+
+
+    
+    
+
+    
+
+    
+    
+    
+
+        
+
+
+
+    
+
+    
+
---
+++ PythonHomework/PyPoll/PyPoll.py
@@ -0,0 +1,97 @@
+import os
+import csv
+
+election_data_csv = os.path.join("..", "..", "PythonHomework", "PyPoll", "election_data.csv")
+
+with open(election_data_csv, 'r', newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile)
+    ###for row in csvreader:
+        ###print(row['Voter ID'])
+
+# Ask 1 : Get total tumber of votes cast
+    row_count = sum(1 for row in csvreader)
+    ###print(row_count)
+
+
+with open(election_data_csv, 'r', newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile)
+
+# Ask 2 : A complete list of candidates that received votes
+    
+    # parse 'Candidates' from dict as string list
+    candidates_list = [str(row['Candidate']) for row in csvreader]
+    ###print candidates_list
+    
+    # call each candidate name from list using 'set', order set so its not random every run
+    candidates = set(candidates_list)
+    sorted_candidates = sorted(candidates)
+    ###print(sorted_candidates)
+
+    # return 'sorted set' to 'list'
+    new_candidate_list = list(sorted_candidates)
+    ###print(new_candidate_list)
+
+# Ask 4 : Get total number of votes per candidate
+
+    # use 'counter' to return dict of 'candidate : votes' from raw candidates list
+    from collections import Counter
+    candidate_vote_count = Counter(candidates_list)
+    ###print(candidate_vote_count)
+
+# Ask 3 : Get percent of votes each candidate won
+
+    # Get each candidates vote count from 'candidate_vote_count', divide by total votes cast
+    khan_percentage = round((candidate_vote_count['Khan'] / row_count)*100, 0)
+    ###print(khan_percentage)
+    correy_percentage = round((candidate_vote_count['Correy'] / row_count)*100, 0)
+    ###print(correy_percentage)
+    otooley_percentage = round((candidate_vote_count["O'Tooley"] / row_count)*100, 0)
+    ###print(otooley_percentage)
+    li_percentage = round((candidate_vote_count['Li'] / row_count)*100, 0)
+    ###print(li_percentage)
+
+# Ask 5 : Determine winner of election by popular vote
+
+    # Use max of candidate vot count dict
+    winner = max(candidate_vote_count, key=candidate_vote_count.get)
+    ###print(winner)
+
+# ---------------------------------------------------------------------------------------------
+
+# Print output to terminal
+print('')
+print('Election Results')
+print('----------------------')
+print('Total Votes: ' + str(row_count))
+print('----------------------')
+print(new_candidate_list[1] + ': ' + str(khan_percentage) + ' ' + '(' + str(candidate_vote_count['Khan']) + ')')
+print(new_candidate_list[0] + ': ' + str(correy_percentage) + ' ' + '(' + str(candidate_vote_count['Correy']) + ')')
+print(new_candidate_list[2] + ': ' + str(li_percentage) + ' ' + '(' + str(candidate_vote_count['Li']) + ')')
+print(new_candidate_list[3] + ': ' + str(otooley_percentage) + ' ' + '(' + str(candidate_vote_count["O'Tooley"]) + ')')
+print('')
+
+
+# Print output to TextFile
+import sys
+sys.stdout=open("output.txt","w")
+print('')
+print('Election Results')
+print('----------------------')
+print('Total Votes: ' + str(row_count))
+print('----------------------')
+print(new_candidate_list[1] + ': ' + str(khan_percentage) + ' ' + '(' + str(candidate_vote_count['Khan']) + ')')
+print(new_candidate_list[0] + ': ' + str(correy_percentage) + ' ' + '(' + str(candidate_vote_count['Correy']) + ')')
+print(new_candidate_list[2] + ': ' + str(li_percentage) + ' ' + '(' + str(candidate_vote_count['Li']) + ')')
+print(new_candidate_list[3] + ': ' + str(otooley_percentage) + ' ' + '(' + str(candidate_vote_count["O'Tooley"]) + ')')
+print('')
+sys.stdout.close()
+
+
+
+
+    
+
+
+    
+    
+    
